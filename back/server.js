1f0eea6c21bb606c5d122c0467d4a8ad118cb6a0
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-11-15 13:48:31 +0100

    dev: added nodemon

--- back/server.js
+++ back/server.js
@@ -1,1 +1,26 @@
-console.log("About to start the server...");
+const express = require("express");
+const serveIndex = require("serve-index");
+
+const app = express();
+const port = 3000;
+
+app.use((req, res, next) => {
+  console.log("req: ", req.url);
+  next();
+});
+
+app.use("/api/date", (req, res) => {
+  res.json({ date: new Date() });
+});
+
+app.use(express.static("."));
+app.use(
+  serveIndex(".", {
+    icons: true,
+  })
+);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+console.log("about to start the web server");
