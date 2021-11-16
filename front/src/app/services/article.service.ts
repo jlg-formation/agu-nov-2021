b874c5bff7bcb4ae37bf90d8185cd470a714b554
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-11-16 17:18:31 +0100

    dev: added localstorage

--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -5,11 +5,28 @@ import { Article } from '../interfaces/article';
   providedIn: 'root',
 })
 export class ArticleService {
-  articles: Article[] = [
-    { name: 'Tournevis cruciforme', price: 2.4, qty: 123 },
-    { name: 'Pince', price: 5, qty: 68 },
-    { name: 'Marteau', price: 8.54, qty: 5 },
-  ];
+  articles: Article[] = this.getArticles();
 
   constructor() {}
+
+  getArticles() {
+    const str = localStorage.getItem('articles');
+    if (!str) {
+      return [
+        { name: 'Tournevis cruciforme', price: 2.4, qty: 123 },
+        { name: 'Pince', price: 5, qty: 68 },
+        { name: 'Marteau', price: 8.54, qty: 5 },
+      ];
+    }
+    return JSON.parse(str) as Article[];
+  }
+
+  save() {
+    localStorage.setItem('articles', JSON.stringify(this.articles));
+  }
+
+  add(article: Article) {
+    this.articles.push(article);
+    this.save();
+  }
 }
