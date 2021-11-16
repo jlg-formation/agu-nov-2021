e8150b231dc259c2f2fbacd0f9a94d057baebbbb
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-11-16 11:13:31 +0100

    dev: routing step1
    we add two routes component:
    - legal
    - home

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
+import { HomeComponent } from './routes/home/home.component';
+import { LegalComponent } from './routes/legal/legal.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, LegalComponent],
   imports: [BrowserModule, AppRoutingModule, LayoutModule],
   providers: [],
   bootstrap: [AppComponent],
