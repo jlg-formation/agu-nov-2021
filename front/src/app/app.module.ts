4968c76afea65f60d42010da8715d64161c371ce
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-11-16 16:07:37 +0100

    dev: added currency pipe
    change locale to french
    change currency to euro

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 import { HomeComponent } from './routes/home/home.component';
 import { LegalComponent } from './routes/legal/legal.component';
 
+registerLocaleData(localeFr, 'fr');
+
 @NgModule({
   declarations: [AppComponent, HomeComponent, LegalComponent],
   imports: [BrowserModule, AppRoutingModule, LayoutModule],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'fr' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR' },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
