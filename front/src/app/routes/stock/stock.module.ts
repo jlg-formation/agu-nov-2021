57b280c21db46b756734416a0300357c12a555db
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-11-16 16:35:12 +0100

    dev: added formGroup

--- front/src/app/routes/stock/stock.module.ts
+++ front/src/app/routes/stock/stock.module.ts
@@ -5,17 +5,15 @@ import { StockRoutingModule } from './stock-routing.module';
 import { StockComponent } from './stock.component';
 import { AddComponent } from './add/add.component';
 import { WidgetsModule } from '../../widgets/widgets.module';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    StockComponent,
-    AddComponent
-  ],
+  declarations: [StockComponent, AddComponent],
   imports: [
     CommonModule,
     StockRoutingModule,
-    WidgetsModule
-  ]
+    WidgetsModule,
+    ReactiveFormsModule,
+  ],
 })
-export class StockModule { }
+export class StockModule {}
