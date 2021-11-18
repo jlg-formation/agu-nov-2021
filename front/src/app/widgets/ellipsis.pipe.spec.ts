eb0b9fdfbd3806503f4096697a34b2ed049e3e3c
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-11-18 15:18:50 +0100

    test: add test on ellipsis

--- front/src/app/widgets/ellipsis.pipe.spec.ts
+++ front/src/app/widgets/ellipsis.pipe.spec.ts
@@ -1,8 +1,31 @@
 import { EllipsisPipe } from './ellipsis.pipe';
 
 describe('EllipsisPipe', () => {
-  it('create an instance', () => {
+  it('should add ... to a long string', () => {
     const pipe = new EllipsisPipe();
-    expect(pipe).toBeTruthy();
+    const string = '' + Math.floor(Math.random() * 1e24);
+    const result = pipe.transform(string);
+    expect(result).toBe(string.substr(0, 10) + '...');
+  });
+
+  it('should add ... to a long string > 15', () => {
+    const pipe = new EllipsisPipe();
+    const string = '' + Math.floor(Math.random() * 1e24);
+    const result = pipe.transform(string, 15);
+    expect(result).toBe(string.substr(0, 15) + '...');
+  });
+
+  it('should return the same string if the string is short', () => {
+    const pipe = new EllipsisPipe();
+    const string = '' + Math.floor(Math.random() * 1e5);
+    const result = pipe.transform(string);
+    expect(result).toBe(string.substr(0, 10));
+  });
+
+  it('should return the same string if the string is short < 15', () => {
+    const pipe = new EllipsisPipe();
+    const string = '' + Math.floor(Math.random() * 1e12);
+    const result = pipe.transform(string, 15);
+    expect(result).toBe(string.substr(0, 15));
   });
 });
